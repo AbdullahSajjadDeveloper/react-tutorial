dbb0770608f02e340a19f8f9097e089893482efc
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-08-11 05:33:53 +0500

    Create 001.whatIsReact.js

---
+++ 001.whatIsReact.js
@@ -0,0 +1,7 @@
+/*
+  What is React.JS?
+    - React.JS is a JavaScript Library for building web-interfaces.
+    - It is developed by Facebook.
+    - It is based on Component-Based approach.
+    - We can use JSX (JavaScript and XML) in it.
+*/
