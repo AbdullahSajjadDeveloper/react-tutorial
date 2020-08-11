41f7d76eff92b74e231e01a6110455c134ffaff2
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-08-11 06:21:18 +0500

    Create 003.virtualDom.js

---
+++ 003.virtualDom.js
@@ -0,0 +1,7 @@
+/*
+  React.JS is based on Virtual DOM.
+  
+  What is Virtual DOM?
+    - That’s where the concept of virtual DOM comes in and performs significantly better than the real DOM. The virtual DOM is only a virtual representation of the DOM.
+      Everytime the state of our application changes, the virtual DOM gets updated instead of the real DOM.
+*/
