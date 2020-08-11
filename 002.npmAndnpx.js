7f3948c4d44ee9f46010fac9afd55813059a8921
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-08-11 06:00:35 +0500

    Create 002.npmAndnpx.js

---
+++ 002.npmAndnpx.js
@@ -0,0 +1,19 @@
+/*
+  What is npm?
+    - It is a tool for installing packages.
+    - It installs packages globally then you can install any packages locally.
+    
+ What is npx?
+    - It is a tool for installing packages.
+    - It installs packages only locally.
+    
+ How to install react and create react app using npm?
+  - First install Globally:-
+    - npm install -g create-react-app
+  - Then create your React Project Locally:-
+    - create-react-app react-tutorial
+    
+ How to install react and create react app using npx?
+  - Directly create a react app locally:-
+    - npx create-react-app react-tutorial
+*/
