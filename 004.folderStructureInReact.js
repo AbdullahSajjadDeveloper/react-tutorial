7e2dd973f277760ab108758fe2c54713c9e0e193
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-08-11 06:54:30 +0500

    Create 004.folderStructureInReact.js

---
+++ 004.folderStructureInReact.js
@@ -0,0 +1,11 @@
+/*
+  So, let us understand the folder structure of our React.JS app:-
+    - folder > node_modules:- It is the folder which contains all the packages of node that we need for our React.JS project.
+    - file > package.json:- It contains the configuration of our React.JS project.
+    - file > package-lock.json:- It contains the dependencies of the node-packages that we are using in our project.
+    - file > public/index.html:- It is the main file which will run on the browser.
+    - file > src/index.js:- It is the main file which will contains dom and component handling, etc.
+    - file > src/app.js:- It is the component file. We can create component in this file. We can also create components in the other files.
+    - file > src/app.css:- It contains the style of the app.js component.
+    - file > src/index.css:- It contains the style of the public/index.html file.
+*/
