c366fd1eaf7a1cda9e17e7e2a620a8a8b8b201c6
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-08-12 17:05:39 +0500

    Create 013.normalOrArrowFunctions.js

---
+++ 013.normalOrArrowFunctions.js
@@ -0,0 +1,6 @@
+// Code:-
+
+/*
+  See what to choose and when to choose Here:-
+    - https://medium.com/better-programming/javascript-whats-the-difference-between-normal-and-arrow-functions-74c367324ae1
+*/
