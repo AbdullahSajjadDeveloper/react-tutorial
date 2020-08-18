e6b187970f0d82bdc85dc19c791270ee0059d64e
Abdullah Sajjad <67973697+AbdullahSajjadDeveloper@users.noreply.github.com>
2020-08-18 20:04:19 +0500

    Create 036.hooksInReact.jsx

---
+++ 036.hooksInReact.jsx
@@ -0,0 +1,6 @@
+// Code:-
+
+/*
+  What are Hooks?
+    - Hooks are for functional components, they are used to implement all those features in functional components that we use in class components.
+*/
